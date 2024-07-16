eed955db96ce9b2bc055d7b1586eb795a618c46f
Armx <114233771+Armx-123@users.noreply.github.com>
2024-07-16 09:05:28 +0530

    Update banner_main.py

--- banner_main.py
+++ banner_main.py
@@ -1,4 +1,5 @@
 import subprocess
-
+subprocess.run(["python", "Python Files/create.py"], check=True)
+subprocess.run(["python", "Python Files/files.py"], check=True)
 subprocess.run(["python", "Python Files/banner/banner.py"], check=True)
 subprocess.run(["python", "Python Files/banner/update_banner.py"], check=True)
