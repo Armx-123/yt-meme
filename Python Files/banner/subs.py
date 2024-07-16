bed5588484afe3fcd50762612e9557fcc0e2c678
Armx <114233771+Armx-123@users.noreply.github.com>
2024-07-16 08:59:48 +0530

    Update subs.py

--- Python Files/banner/subs.py
+++ Python Files/banner/subs.py
@@ -1,4 +1,5 @@
 import googleapiclient.discovery
+import os
 
 def get_subscriber_count(api_key, channel_id):
     # Create a resource object for interacting with the API
@@ -15,13 +16,14 @@ def get_subscriber_count(api_key, channel_id):
     if 'items' in response and len(response['items']) > 0:
         subscriber_count = response['items'][0]['statistics']['subscriberCount']
         return subscriber_count
+        print(subscriber_count)
     else:
         return 'Channel not found'
 
 # Replace with your API key and the channel ID
-# api_key = 'AIzaSyD27p8iQO1g8iaazCctPbnWyheNXcefNfc'
-# channel_id = 'UCT0vmR76yrI__1W2W6ELMxA'
+api_key = os.environ['API']
+channel_id = 'UCWmLyLVWoHZdz70Fpt2g6UQ'
 
-# subscriber_count = get_subscriber_count(api_key, channel_id)
-# print(f'The latest subscriber count is: {subscriber_count}')
+subscriber_count = get_subscriber_count(api_key, channel_id)
+print(f'The latest subscriber count is: {subscriber_count}')
 
