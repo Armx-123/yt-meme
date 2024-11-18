09a37423f35ff4c745e1dea92c414fc58a3218ce
Armx <114233771+Armx-123@users.noreply.github.com>
2024-11-18 18:54:32 +0530

    Delete banner_main.py

--- banner_main.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-subprocess.run(["python", "Python Files/create.py"], check=True)
-subprocess.run(["python", "Python Files/files.py"], check=True)
-subprocess.run(["python", "Python Files/banner/banner.py"], check=True)
-subprocess.run(["python", "Python Files/banner/update_banner.py"], check=True)
